5125b7646549fbf75881dd05da666ee7afea2e2f
michael_wolf <michael.wolf@atraveo.de>
2017-01-26 20:40:24 +0100

    Namespaced communication protocol

--- public/js/websocket.js
+++ public/js/websocket.js
@@ -3,7 +3,7 @@ var socket = io();
 socket.on('connected', function() {
     console.log('connected to backend');
 
-    socket.emit('command', 'newsGetList', {items: 10, page: 0});
+    socket.emit('command', 'news.getList', {items: 10, page: 0});
 });
 socket.on('socketError', function(error) {
     console.log('socketError: ', error);
@@ -12,11 +12,11 @@ socket.on('invalidRequest', function(error) {
     console.log('invalidRequest: ', error);
 });
 
-socket.on('ownUser', function(data) {
+socket.on('user.ownDetails', function(data) {
     $('#navUser').text('Logged in as ' + data.displayName)
         .attr('data-userid', data.id);
 });
 
-socket.on('newsList', function(data) {
+socket.on('news.list', function(data) {
     console.log("News: ", data);
 });
