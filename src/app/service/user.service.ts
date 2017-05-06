28e4bc69d9bf7f7a11929e089e9ad46897b320f6
michael_wolf <michael.wolf@atraveo.de>
2017-05-06 17:29:33 +0200

    Current Match of the user will now be transfered to the frontend and displayed in the navbar.

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import {Subscription} from "rxjs/Subscription";
 
 import { CommunicationService } from './communication.service';
 import { CookieService } from 'angular2-cookie/core';
@@ -13,6 +14,7 @@ export class UserService {
     userSubject:BehaviorSubject<User> = new BehaviorSubject(this.user);
     statusSubject:BehaviorSubject<string> = new BehaviorSubject(null);
     userSession:any = null;
+    private subscriptions:Subscription[] = [];
 
     constructor(private communicationService: CommunicationService, private cookieService: CookieService) {
         this.userSession = this.cookieService.getObject('userSession');
@@ -97,11 +99,21 @@ export class UserService {
                 this.userSubject.next(this.user);
 
                 this.cookieService.remove('userSession');
+
+
+                for (let subscription of this.subscriptions) {
+                    subscription.unsubscribe();
+                }
             }
         });
+
     }
 
     private setLoggedinUser(data: any): void {
+        for (let subscription of this.subscriptions) {
+            subscription.unsubscribe();
+        }
+
         this.user = new User();
         this.user.id = data.id;
         this.user.displayName = data.displayName;
@@ -110,8 +122,18 @@ export class UserService {
 
         this.userSubject.next(this.user);
 
+        let currentDate = new Date();
+        let cookieDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + 7);
+
         this.userSession = {userId: this.user.id, authToken: data.authToken};
-        this.cookieService.putObject('userSession', this.userSession);
-        this.cookieService.putObject('username', this.user.displayName);
+        this.cookieService.putObject('userSession', this.userSession, {expires: cookieDate});
+        this.cookieService.putObject('username', this.user.displayName, {expires: cookieDate});
+
+
+        this.subscriptions.push(this.communicationService.listen('user.currentMatchChanged').subscribe(data => {
+            if (data.id != this.user.id) return;
+
+            this.user.currentMatch = data.data.currentMatch;
+        }));
     }
 }
